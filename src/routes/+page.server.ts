e4377e92951c2217c6a8c005b66b3198e7819827
JCEEE <0xjceee@proton.me>
2025-02-27 17:36:44 +0000

    debug typo

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import PocketBase from 'pocketbase';
-import { PB_USER, PB_SECRET } from '$env/staic/private';
+import { PB_USER, PB_SECRET } from '$env/static/private';
 const ENDPOINT = process.env?.npm_package_config_ENDPOINT;
 const COLLECTION = process.env.npm_package_config_COLLECTION!;
 const pb = new PocketBase('https://pb.cycocyan.xyz');
