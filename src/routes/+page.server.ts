5d6993521b18e07a4b13c912d852d9bcf3e23efa
JCEEE <0xjceee@proton.me>
2025-02-27 17:36:02 +0000

    debug

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,14 @@
+// @ts-nocheck
 import PocketBase from 'pocketbase';
-import { PB_USER, PB_SECRET } from '$env/dynamic/private';
+import { PB_USER, PB_SECRET } from '$env/staic/private';
 const ENDPOINT = process.env?.npm_package_config_ENDPOINT;
 const COLLECTION = process.env.npm_package_config_COLLECTION!;
-const pb = new PocketBase(ENDPOINT);
+const pb = new PocketBase('https://pb.cycocyan.xyz');
 await pb.collection('users').authWithPassword(PB_USER, PB_SECRET);
 
 export async function load() {
-	const collection: Site[] = await pb.collection(COLLECTION).getFullList({ sort: '-updated' });
+	const collection: Site[] = await pb
+		.collection('Microsite_Meta')
+		.getFullList({ sort: '-updated' });
 	return { collection };
 }
