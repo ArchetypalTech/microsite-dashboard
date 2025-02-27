242475627dda9aca02f0c45bf528a2d833e97ba4
JCEEE <0xjceee@proton.me>
2025-02-27 17:28:38 +0000

    try dyamic env

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
-import { PB_USER, PB_SECRET } from '$env/static/private';
+import { PB_USER, PB_SECRET } from '$env/dynamic/private';
 const ENDPOINT = process.env?.npm_package_config_ENDPOINT;
 const COLLECTION = process.env.npm_package_config_COLLECTION!;
 const pb = new PocketBase(ENDPOINT);
